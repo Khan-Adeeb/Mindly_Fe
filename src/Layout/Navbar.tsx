a0efb33f83983988ac8e21c2fbd5f74284e4c3ee
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-23 03:20:47 +0530

    nav ui complete

---
+++ src/Layout/Navbar.tsx
@@ -0,0 +1,21 @@
+import { Globe, LogOut } from "lucide-react";
+import Button from "../components/Button";
+
+export default function Navbar() {
+  return (
+    <div className="fixed top-0 left-0 w-screen h-14 bg-white border-b border-zinc-200 p-3">
+      <div className="main flex justify-between items-center">
+        <div className="logo flex items-center gap-2">
+          <p className="text-white bg-blue-600 rounded-lg px-2.5 py-1 font-bold"> M </p>
+          <p className="font-bold text-zinc-800">Mindly</p>
+        </div>
+        <div className="buttons flex items-center gap-3">
+          <Button size="sm" variant="secondary" startIcon={<Globe size={16}/>} text="Share Brain" />
+          <p className="text-sm font-normal text-zinc-700 mx-2">Adeeb</p>
+          <Button size="sm" variant="ghost" startIcon={<LogOut size={16}/>} text="Logout" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
