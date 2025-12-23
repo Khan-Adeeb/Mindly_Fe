e8747606a2b5b609f05ec5e67f5d12fb200b9f80
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-24 02:25:48 +0530

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { Plus } from 'lucide-react'
-import Button from './components/Button'
-
+import Routing from "./Router/Routing";
 function App() {
-  return (
-    <div className='flex-col gap-4  p-5' >
-      
-      <Button variant='ghost' startIcon={<Plus size={12}/>} text='Hello World' size='sm'/>
-      <Button variant='ghost' startIcon={<Plus size={14}/>} text='Hello World' size='md'/>
-      <Button variant='ghost' startIcon={<Plus size={16}/>} text='Hello World' size='lg'/>
-    </div>
-  )
+  return <Routing />;
 }
-
-export default App  
+export default App;
