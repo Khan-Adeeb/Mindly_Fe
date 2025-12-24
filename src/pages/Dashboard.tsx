52af400eb94192f583d06ab47ba2cd0180aabe6d
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-24 21:42:16 +0530

    dashboard changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,83 +1,78 @@
+import { useEffect } from "react";
 import Card from "../components/Card";
+import AddContentModal from "../components/modals/AddContentModal";
+import BrainShareModal from "../components/modals/BrainShareModal";
+import ShareComponentModal from "../components/modals/ShareComponentModal";
 import Navbar from "../Layout/Navbar";
 import Sidebar from "../Layout/Sidebar";
-
- const sampleContents = [
-    {
-      _id: "5",
-      title: "System Design Fundamentals",
-      link: "https://youtu.be/K9R7KcaettM",
-      type: "youtube",
-      description: "Complete guide to system design concepts and patterns",
-      tags: [
-        { _id: "9", title: "systemdesign" },
-        { _id: "7", title: "tech" },
-      ],
-      userId: { id: "1", email: "demo@brainly.app", name: "Demo User" },
-      createdAt: new Date(2024, 11, 8).toDateString(),
-    },
-    {
-      _id: "6",
-      title: "React Hooks Tutorial",
-      link: "https://www.youtube.com/watch?v=PIyf0hMc498&list=RDGMEMCMFH2exzjBeE_zAHHJOdxgVMK9R7KcaettM&index=2",
-      type: "youtube",
-      tags: [
-        { _id: "10", title: "react" },
-        { _id: "11", title: "tutorial" },
-      ],
-      userId: { id: "1", email: "demo@brainly.app", name: "Demo User" },
-      createdAt: new Date(2024, 11, 5).toDateString(),
-    },{
-      _id: "5",
-      title: "System Design Fundamentals",
-      link: "https://youtu.be/K9R7KcaettM",
-      type: "twitter",
-      description: "Complete guide to system design concepts and patterns",
-      tags: [
-        { _id: "9", title: "systemdesign" },
-        { _id: "7", title: "tech" },
-      ],
-      userId: { id: "1", email: "demo@brainly.app", name: "Demo User" },
-      createdAt: new Date(2024, 11, 8).toDateString(),
-    },
-    {
-      _id: "6",
-      title: "React Hooks Tutorial",
-      link: "https://www.youtube.com/watch?v=PIyf0hMc498&list=RDGMEMCMFH2exzjBeE_zAHHJOdxgVMK9R7KcaettM&index=2",
-      type: "youtube",
-      tags: [
-        { _id: "10", title: "react" },
-        { _id: "11", title: "tutorial" },
-      ],
-      userId: { id: "1", email: "demo@brainly.app", name: "Demo User" },
-      createdAt: new Date(2024, 11, 5).toDateString(),
-    }
-  ];
+import {
+  useAddModalStore,
+  useAllContentsStore,
+  useBrainShareModalStore,
+  useShareModalStore,
+} from "../Store/store";
 
 
 export default function Dashboard() {
+  const addmodal = useAddModalStore((state) => state.isOpen);
+  const toggleAdd = useAddModalStore((state) => state.toggleModal);
+
+  const sharemodal = useShareModalStore((state) => state.isOpen);
+  const toggleShare = useShareModalStore((state) => state.toggleModal);
+
+  const brainmodal = useBrainShareModalStore((state) => state.isOpen);
+  const toggleBrain = useBrainShareModalStore((state) => state.toggleModal);
+
+  let store = useAllContentsStore();
+  let { contents, fetchContent, isLoading } = store;
+
+  useEffect(() => {
+    fetchContent();
+    let interval = setInterval(() => {
+      fetchContent();
+      console.log(contents);
+      
+    }, 30000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  
   return (
     <>
-     <Navbar />
-      <Sidebar />
-      <div className="main ml-64 pt-14">
+      <Navbar toggleModal={toggleBrain} />
+      <Sidebar toggleModal={toggleAdd} />
+      <div className="main ml-64 pt-14 bg-zinc-50 min-h-screen">
         <div className="inside p-5">
           <div className="title mb-5">
             <p className="text-xl font-medium text-zinc-800">All Content</p>
             <p className="text-sm text-zinc-600">7 items</p>
           </div>
           <div className="card">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-col-3 gap-4">
-              {sampleContents.map((content) => (
-                <Card
-                  key={content._id}
-                  content={content}
-                />
-              ))}
-            </div>
+            {isLoading && <p>Loading... </p>}
+            {
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-col-3 gap-4">
+                {contents.map((content) => (
+                  <Card
+                    key={content._id}
+                    content={content}
+                    onShare={toggleShare}
+                  />
+                ))}
+              </div>
+            }
           </div>
         </div>
       </div>
+
+      <ShareComponentModal
+        link="http://"
+        open={sharemodal}
+        onClose={toggleShare}
+      />
+      <BrainShareModal link="http://" open={brainmodal} onClose={toggleBrain} />
+      <AddContentModal open={addmodal} onClose={toggleAdd} />
     </>
-  )
+  );
 }
