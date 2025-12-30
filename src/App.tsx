a99d54d954cf52cc4a2e3f0f1c4567e78877bdd6
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-30 17:48:36 +0530

    remove unseccasary routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-import Routing from "./Router/Routing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SharedContent from "./pages/SharedContent";
+
 function App() {
-  return <Routing />;
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/shared-content/:hash" element={<SharedContent />} />
+      </Routes>
+    </BrowserRouter>
 }
 export default App;
